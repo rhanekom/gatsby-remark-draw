7fcf99a81e8651afab0f48bd953ba08abdcd1205
Riaan Hanekom <rhanekom@gmail.com>
2018-03-22 13:03:34 +0200

    Tests

--- test/lib/pipedprocess.test.js
+++ test/lib/pipedprocess.test.js
@@ -1,7 +1,19 @@
 const PipedProcess = require('../../lib/pipedprocess');
-const testString = "hello 123 ''";
+const testString = "hello 456 ''";
 
-test('execute returns passes input', () => {
+test("execute passes input and returns output", () => {
     let process = new PipedProcess();
     expect(process.execute('cat', [], testString)).toBe(testString);
+});
+
+test("execute passes parameters", () => {
+    let process = new PipedProcess();
+    let output = process.execute("cat", ["-n"], testString);
+
+    /*  Whitespace
+     *  followed by line number (1)
+     *  followed by whitespace
+     *  followed by test string */
+    let test = new RegExp(`[\\s\\S]*1[\\s\\S]*${testString}$`);
+    expect(output).toMatch (test);
 });
